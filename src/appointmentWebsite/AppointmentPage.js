62f62fcfa4504945111e90fa830acc26c13b7fe9
USMAN-SENG <othmandoloh@gmail.com>
2022-06-10 13:12:25 +0800

    send array to firebase, dashboard calender

--- src/appointmentWebsite/AppointmentPage.js
+++ src/appointmentWebsite/AppointmentPage.js
@@ -1,64 +1,95 @@
-import React from 'react'
-import AppoHeader from './components/AppoHeader'
+import React from "react";
+import AppoHeader from "./components/AppoHeader";
 import { useRef, useState } from "react";
-import Calendar from 'react-calendar';
+import Calendar from "react-calendar";
 import Grid from "@mui/material/Grid";
-import 'react-calendar/dist/Calendar.css';
+import Button from "@mui/material/Button";
+import "react-calendar/dist/Calendar.css";
+import { Scheduler } from "@arshadrao/react-scheduler";
 
 export default function AppointmentPage() {
+	const [todayDate, setTodayDate] = useState(new Date());
+	const [name, setname] = useState("ahmad");
+	const [date, setDate] = useState(new Date());
 
-  const [todayDate, setTodayDate] = useState(new Date());
-  const [name, setname] = useState('ahmad');
-  const [date, setDate] = useState(new Date());
+	function handleDate(date) {
+		setDate(date);
+	}
 
-  function handleDate(date){
-    setDate(date);
-  }
+	// const onChange = date => {
+	//   setDate(date);
+	// };
 
-  // const onChange = date => {
-  //   setDate(date);
-  // };
+	let notIncludeDay = {
+		dayArray: [0, 6, 5],
+	};
+	const disabledDates = [];
+	if (notIncludeDay.dayArray.length) {
+		for (let i = 0; i < 7; i++) {
+			if (i > notIncludeDay.dayArray.length)
+				disabledDates.push(disabledDates[0]);
+			else disabledDates.push(notIncludeDay.dayArray[i]);
+		}
+	}
 
-let notIncludeDay= {
-  dayArray: [0, 6,5]
-}
-const disabledDates = [];
-if(notIncludeDay.dayArray.length){
-  for (let i = 0; i < 7; i++){
-    if(i> notIncludeDay.dayArray.length)
-    disabledDates.push(disabledDates[0]);
-    else 
-    disabledDates.push(notIncludeDay.dayArray[i]);
-  }
-}
+	// sun= 0 , mon = 1 , ...
+	function disableDays({ activeStartDate, date, view }) {
+		// we take date of each not include day and repeate it
+		// we do a for loop to all not include day and then repeat the last value
+		return (
+			date.getDay() === disabledDates[0] ||
+			date.getDay() === disabledDates[1] ||
+			date.getDay() === disabledDates[2] ||
+			date.getDay() === disabledDates[3] ||
+			date.getDay() === disabledDates[4] ||
+			date.getDay() === disabledDates[5] ||
+			date.getDay() === disabledDates[6]
+		); // sun , saturday
+	}
 
+	function generateButton () {
+		for (var i = 0; i <3 ; i++) {
+			return <Button color="inherit">Book</Button> ;
+		}
+	}
 
-// sun= 0 , mon = 1 , ...
-function disableDays({activeStartDate, date, view }) 
-{ // we take date of each not include day and repeate it
-  // we do a for loop to all not include day and then repeat the last value
-  return date.getDay() === disabledDates[0] || date.getDay() === disabledDates[1] || date.getDay() === disabledDates[2]  || date.getDay() === disabledDates[3]|| date.getDay() === disabledDates[4]|| date.getDay() === disabledDates[5]|| date.getDay() === disabledDates[6] ; // sun , saturday
-}
+	return (
+		<>
+			<AppoHeader />
 
-  return (
-    <>
-      <AppoHeader />
-     
-      {/* <div>helloe </div>
+			{/* <div>helloe </div>
       <div>{name} </div>
       <div>{todayDate.toString()} </div> */}
-      <Grid container justifyContent="center"
-			alignItems="center" m={5}>
-        <Grid item  >
-        
-      <Calendar  tileDisabled={disableDays} showDoubleView={true} onChange={handleDate} value={date} />
-      {console.log(date)}
-      {date.toString()}
-    
-        </Grid>
-      </Grid>
-      
-    
-    </>
-  )
+			<Grid container  m={5}>
+				<Grid item>
+					<Calendar
+						tileDisabled={disableDays}
+						onChange={handleDate}
+						value={date}
+					/>
+					{date.toString()}
+					
+				</Grid>
+				<Grid item> hellow </Grid>
+			</Grid>
+			<Scheduler
+  view="month"
+  events={[
+    {
+      event_id: 1,
+      title: "i want to renew my visa , i had problems for months ",
+      start: new Date("2022/6/11 09:30"),
+      end: new Date("2022/6/11 14:30"),
+    },
+    {
+      event_id: 2,
+      title: "Event 2",
+      start: new Date("2022/6/11 16:30"),
+      end: new Date("2022/6/11 17:30"),
+    },
+  ]}
+/>
+		
+		</>
+	);
 }
--- src/firebase.js
+++ src/firebase.js
@@ -78,9 +78,83 @@ arrayTest.push(car);
 let objectWithArray = {
 	array: arrayTest,
 };
+
+const EventObjInArr = [
+  {
+    event_id: 1,
+    title: "Event 1",
+    start: new Date("2021 5 2 9:30"),
+    end: new Date("2021 5 2 10:30")
+  },
+  {
+    event_id: 2,
+    title: "Event 2",
+    start: new Date("2021 5 4 10:00"),
+    end: new Date("2021 5 4 11:00")
+  },
+  {
+    event_id: 3,
+    title: "Event 3",
+    start: new Date("2021 4 27 09:00"),
+    end: new Date("2021 4 28 10:00")
+  },
+  {
+    event_id: 4,
+    title: "Event 4",
+    start: new Date("2021 5 4 9:00"),
+    end: new Date("2021 5 4 10:36")
+  },
+  {
+    event_id: 5,
+    title: "Event 5",
+    start: new Date("2021 5 1 10:00"),
+    end: new Date("2021 5 18 11:00")
+  },
+  {
+    event_id: 6,
+    title: "Event 6",
+    start: new Date("2021 5 2 11:00"),
+    end: new Date("2021 5 2 12:00")
+  },
+  {
+    event_id: 7,
+    title: "Event 7",
+    start: new Date("2021 5 1 12:00"),
+    end: new Date("2021 5 1 13:00")
+  },
+  {
+    event_id: 8,
+    title: "Event 8",
+    start: new Date("2021 5 1 13:00"),
+    end: new Date("2021 5 1 14:00")
+  },
+  {
+    event_id: 9,
+    title: "Event 11",
+    start: new Date("2021 5 5 16:00"),
+    end: new Date("2021 5 5 17:00")
+  },
+  {
+    event_id: 10,
+    title: "Event 9",
+    start: new Date("2021 5 6  15:00"),
+    end: new Date("2021 5 6 16:00")
+  },
+  {
+    event_id: 11,
+    title: "Event 10",
+    start: new Date("2021 5 6 14:00"),
+    end: new Date("2021 5 6 15:00")
+  }
+];
+
+let objectWithArray2 = {
+	arrayTest: EventObjInArr,
+};
 //can't send an array, have to be an object
 // we have to do a nested object
-
+// no need for nested object , when you create object with array inside it , it will only send the array 
 export function addTestArray() {
-	return addDoc(usersColRef, objectWithArray); // addDoc will create random id
+	return addDoc(usersColRef, objectWithArray2); // addDoc will create random id
 }
+
